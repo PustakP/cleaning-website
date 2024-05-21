cf9a09d791eb0ce12c6fb0f0ded9f824df0d69c0
Proxyy <121728080+proxyingithub@users.noreply.github.com>
2024-05-21 16:42:12 +0530

    third

--- components/shared/WorkingSection.tsx
+++ components/shared/WorkingSection.tsx
@@ -1,27 +1,38 @@
-import { HowitWorks } from "@/constants"
-import { HowItWorks } from "@/components/shared/how-it-works"
+import { HowitWorks } from "@/constants";
+import { HowItWorks } from "@/components/shared/how-it-works";
+import Image from "next/image";
 
 const WorkingSection = () => {
   return (
-    <section className='w-full bg-zinc-200/40 h-screen mt-20 px-6 md:px-24 py-24'>
-        <div className="pb-4">
-            <h1 className='text-5xl font-bold text-sky-950 mb-1 text-center md:text-start'>How it Works</h1>
-            <p className='text-sm md:text-lg text-center md:text-start font-medium text-gray-400'>What can you expect when you order from us</p>
-            </div>
-
-            <div>
-            {
-                HowitWorks.map((item)=> (
-                    <HowItWorks key={item.title} title={item.title} description={item.description} icon={item.icon} />
-                ))
-            }
+    <section className="w-full flex flex-col lg:flex-row justify-between items-center bg-zinc-200/40 mt-20 px-6 md:px-24 py-24">
+      <div>
+        <div className="pb-12">
+          <h1 className="text-5xl font-bold text-sky-950 mb-1 text-center md:text-start">
+            How it Works
+          </h1>
+          <p className="text-sm md:text-lg text-center md:text-start font-medium text-gray-400">
+            What can you expect when you order from us
+          </p>
         </div>
 
-        <div>
-            
+        <div className="pb-8 gap-8 md:gap-0 flex flex-col">
+          {HowitWorks.map((item) => (
+            <HowItWorks
+              key={item.title}
+              title={item.title}
+              description={item.description}
+              icon={item.icon}
+            />
+          ))}
         </div>
+      </div>
+
+      <div className="">
+        <Image src={"/houseimage.jpg"} alt="" width={500} height={500} className="aspect-square"/>
+        {/* <Image src={"/cleaning.jpg"} alt="" width={600} height={630} className=""/> */}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default WorkingSection
+export default WorkingSection;
